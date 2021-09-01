fb4318814ae55f18c58f7a42475ad2c99bd7efa4
zxc8027 <zxc8027@g.rit.edu>
2021-08-31 23:26:40 -0400

    Create initial database configuration.

---
+++ Construct.Core/Configuration/ConstructConfiguration.cs
@@ -0,0 +1,74 @@
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Construct.Core.Configuration
+{
+    public class Database
+    {
+        /// <summary>
+        /// Database provider to use.
+        /// </summary>
+        public string Provider { get; set; } = "sqlite";
+        
+        /// <summary>
+        /// Non-default data source to use. Can be a URL or a file name.
+        /// </summary>
+        public string Source { get; set; }
+        
+        /// <summary>
+        /// Username to access the data source.
+        /// </summary>
+        public string Username { get; set; }
+        
+        /// <summary>
+        /// Password to access the data source.
+        /// </summary>
+        public string Password { get; set; }
+    }
+    
+    public class ConstructConfiguration
+    {
+        /// <summary>
+        /// Default file location of the configuration.
+        /// </summary>
+        public const string FileLocation = "configuration.json";
+
+        /// <summary>
+        /// Static configuration used for the application.
+        /// </summary>
+        public static ConstructConfiguration Configuration = new ConstructConfiguration();
+
+        /// <summary>
+        /// Database configuration of the application.
+        /// </summary>
+        public Database Database { get; set; } = new Database();
+
+        /// <summary>
+        /// Loads a configuration.
+        /// </summary>
+        /// <param name="fileLocation">Location of the configuration.</param>
+        /// <returns>The loaded configuration.</returns>
+        public static async Task<ConstructConfiguration> LoadAsync(string fileLocation = FileLocation)
+        {
+            // Return an empty configuration if none exists.
+            if (!File.Exists(fileLocation))
+            {
+                return new ConstructConfiguration();
+            }
+            
+            // Read and return the configuration.
+            var configurationData = await File.ReadAllTextAsync(fileLocation);
+            return JsonSerializer.Deserialize<ConstructConfiguration>(configurationData);
+        }
+
+        /// <summary>
+        /// Loads the static configuration.
+        /// </summary>
+        /// <param name="fileLocation">Location of the configuration.</param>
+        public static async Task LoadDefaultAsync(string fileLocation = FileLocation)
+        {
+            Configuration = await LoadAsync(fileLocation);
+        }
+    }
+}
